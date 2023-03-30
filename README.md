1d094a2eb7652c6bfb3e1608eb38a484a2440005
JP van Loggerenberg <jploggies@gmail.com>
2023-03-30 22:40:56 +0200

    Mint, View, Transfer with Readme

--- README.md
+++ README.md
@@ -8,12 +8,36 @@ Ensure you have: npm install -g near-cli
 To store your non-fungible tokens you'll need a NEAR Wallet. If you don't have one yet, you can create one easily by following these [instructions](https://wallet.testnet.near.org/create). 
 
 # Building the contract
-To build your contract run the following command in your terminal which builds your contract using Rust's cargo.
+To build your contract run the following command in your terminal which builds your contract using Rust's cargo. 
 
-## Windows 
+## Build on Windows 
 ./scripts/build.bat
 
-## Linux 
-./scripts/build.sh
+## Build on Linux 
+./scripts/build.sh 
 
+# Deploying the contract
+This smart contract will be deployed to your NEAR account. Because NEAR allows the ability to upgrade contracts on the same account, initialization functions must be cleared.
 
+### Log in to your newly created account with near-cli by running the following command in your terminal.
+
+near login
+ 
+#### Deploy Command:
+near deploy [accountId] [wasmFile] [initFunction] [initArgs] [initGas] [initDeposit]
+
+e.g. near deploy near deploy --wasmFile res/mysmartcontractbuildfile.wasm --accountId myaccount.testnet
+
+near deploy --wasmFile res/simple_nft_mfg.wasm --initFunction new --initArgs "{\"owner_id\":\"asd.testnet\"}" --accountId asd.testnet
+
+# Test the mint nft function (Note, you will need IPFS storage for reference of the NFT uri)
+ 
+near call asd.testnet mint_nft "{\"token_id\":\"1\",\"receiver_id\":\"asd.testnet\",\"metadata\":{\"title\":\"NAME \",\"description\":\"One of a kind\",\"media\":\"https:\/\/asd.ipfs.nftstorage.link\"}}" --accountId asd.testnet
+
+# View the ownership:
+
+near view asd.testnet get_nft "{\"token_id\": \"1\"}"
+
+# Transfer the ownership:
+
+near call asd.testnet transfer_nft "{\"token_id\": \"1\", \"receiver_id\": \"asd.testnet\"}" --accountId asd.testnet
--- simple_nft/src/lib.rs
+++ simple_nft/src/lib.rs
@@ -1,33 +1,29 @@
+use near_sdk::PanicOnDefault;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{collections::UnorderedMap, near_bindgen, AccountId, Promise};
+use near_sdk::{collections::UnorderedMap, near_bindgen, AccountId, Promise, env};
 use serde::{Deserialize, Serialize};
  
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
-pub struct NFTMetadata {
+pub struct metadata {
     pub title: String,
     pub description: String,
-    pub uri: String,
+    pub media: String,
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct SimpleNFT {
     nfts: UnorderedMap<String, AccountId>,
-    metadata: UnorderedMap<String, NFTMetadata>,
+    metadata: UnorderedMap<String, metadata>,
     owner_id: AccountId,
 }
-
-impl Default for SimpleNFT {
-    fn default() -> Self {
-        panic!("SimpleNFT should be initialized before usage")
-    }
-}
-
+ 
 #[near_bindgen]
 impl SimpleNFT {
     #[init]
     pub fn new(owner_id: AccountId) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
         Self {
             nfts: UnorderedMap::new(b"nfts".to_vec()),
             metadata: UnorderedMap::new(b"metadata".to_vec()),
@@ -35,19 +31,14 @@ impl SimpleNFT {
         }
     }
 
-    pub fn mint_nft(
-        &mut self,
-        token_id: String,
-        receiver_id: AccountId,
-        nft_metadata: NFTMetadata,
-    ) {
-        assert_eq!(self.owner_id, near_sdk::env::predecessor_account_id());
+    pub fn mint_nft(&mut self, token_id: String, receiver_id: AccountId, metadata: metadata) {
+        assert_eq!(&self.owner_id, &env::predecessor_account_id(), "Only the owner can mint tokens");
         self.nfts.insert(&token_id, &receiver_id);
-        self.metadata.insert(&token_id, &nft_metadata);
+        self.metadata.insert(&token_id, &metadata);
     }
 
     pub fn transfer_nft(&mut self, token_id: String, receiver_id: AccountId) {
-        assert_eq!(self.owner_id, near_sdk::env::predecessor_account_id());
+        assert_eq!(&self.owner_id, &env::predecessor_account_id(), "Only the owner can transfer tokens");
         let owner = self.nfts.get(&token_id).expect("NFT not found");
         assert_eq!(
             owner,
@@ -58,7 +49,7 @@ impl SimpleNFT {
         self.nfts.insert(&token_id, &receiver_id);
     }
 
-    pub fn get_nft(&self, token_id: String) -> Option<(AccountId, NFTMetadata)> {
+    pub fn get_nft(&self, token_id: String) -> Option<(AccountId, metadata)> {
         let owner = self.nfts.get(&token_id)?;
         let metadata = self.metadata.get(&token_id)?;
         Some((owner, metadata))
